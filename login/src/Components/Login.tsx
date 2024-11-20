c2ebe5b03d719701187b7b5145cc5766ff255781
manasa-randstad <manasa.j@randstaddigital.com>
2024-11-20 17:23:25 +0530

    Login Form HTML

--- login/src/Components/Login.tsx
+++ login/src/Components/Login.tsx
@@ -2,9 +2,22 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>
-        <h3 className="text-3xl font-bold underline">This is Login Page</h3>  
-    </div>
+    <>
+        <h1>Login Form</h1>
+        <form className="login-form flex flex-col items-center gap-2 p-4 m-4">
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" />
+        </div>
+        <div>            
+          <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" />
+        </div>
+
+            <button type="submit">Login</button>
+        </form>
+          
+    </>
   )
 }
 
