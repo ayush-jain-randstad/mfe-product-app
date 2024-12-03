5b76c61fef75e64fb6997d64d44afb6b914b4a90
manasa-randstad <manasa.j@randstaddigital.com>
2024-12-03 16:34:18 +0530

    Main app CSS, LOgin Redirect and few CSS issues

--- login/src/Components/AuthLables.tsx
+++ login/src/Components/AuthLables.tsx
@@ -10,7 +10,7 @@ function AuthLables({loginLable, setLogin}: AuthLablesProps) {
   return (
     <div className="authLables flex gap-4 items-center text-md font-bold m-1 p-1 w-full">
         <button className={loginLable ? 'bg-blue-600 p-1 rounded w-1/2 border-2 text-white rounded hover:bg-blue-500' : 'w-1/2 border-2 p-1 rounded'} onClick={() => { loginLable ? setLogin(false) : setLogin(true)}}>Member Login</button>
-        <button className={!loginLable ? 'bg-blue-600 p-1 rounded w-1/2 border-2 text-white rounded hover:bg-blue-500' : 'w-1/2 border-2 p-1 rounded'} onClick={() => { loginLable ? setLogin(false) : setLogin(true)}}>Register</button>
+        <button id = "register" className={!loginLable ? 'bg-blue-600 p-1 rounded w-1/2 border-2 text-white rounded hover:bg-blue-500' : 'w-1/2 border-2 p-1 rounded'} onClick={() => { loginLable ? setLogin(false) : setLogin(true)}}>Register</button>
     </div>
   )
 }
